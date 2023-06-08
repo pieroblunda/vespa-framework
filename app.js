1e4a79cafe1547788afa91f647b992949260dad8
pieroblunda <pieroblunda@gmail.com>
2023-06-08 14:58:18 +0200

    Fix

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import dotenv from 'dotenv';
-import Meanivan from './framework/meanivan.js';
 import * as server from './framework/server.js'
 
 let envDotFile = dotenv.config();
 
 process.unhandledRejections = 'strict';
-Meanivan.setGlobals();
 
 server.init(envDotFile.parsed);
 
