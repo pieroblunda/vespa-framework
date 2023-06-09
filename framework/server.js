69e3f056c09eb2108f3338154f50f6369e7a034d
pieroblunda <pieroblunda@gmail.com>
2023-06-09 12:25:29 +0200

    Add support for ToDoClie

--- framework/server.js
+++ framework/server.js
@@ -1,4 +1,5 @@
 'use strict';
+import TodoCli from 'technical-debt-cli';
 import Framework from '../framework/framework.js';
 import DbClient from './dbclient.js';
 import Routes from '../server/routes.js';
@@ -18,10 +19,25 @@ class Server {
     await DbClient.connect(process.env.CONNECTION_STRING);
     console.log('Server connected to DB');
     
-    app.listen(process.env.PORT, () => {
-      console.log('Server started')
+    return new Promise( (resolve, reject) => {
+      app.listen(process.env.PORT, () => {
+        console.log('Server started');
+        this.onAfterStart();
+        resolve(this);
+      });
     });
   }
+  
+  static onAfterStart() {
+    // Print Todo
+    if (process.env.NODE_ENV === 'development') {
+      TodoCli.init({
+        target: ['client/**/*.{js,styl}', 'server/**/*.js'],
+        verbose: false
+      });
+    }
+  }
+  
   
 }
 
