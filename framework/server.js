69f2f3a1a55cd2a7665a2ceaf4a46210a5c16ce0
pieroblunda <pieroblunda@gmail.com>
2023-06-09 12:50:13 +0200

    Fix dynamic import

--- framework/server.js
+++ framework/server.js
@@ -2,8 +2,6 @@
 import Framework from '../framework/framework.js';
 import DbClient from './dbclient.js';
 import Routes from '../server/routes.js';
-let TodoCli = await import('technical-debt-cli'); // Dynamic import
-TodoCli = TodoCli.default;
 
 class Server {
   
@@ -29,9 +27,11 @@ class Server {
     });
   }
   
-  static onAfterStart() {
+  static async onAfterStart() {
     // Print Todo
     if (process.env.NODE_ENV === 'development') {
+      let TodoCli = await import('technical-debt-cli'); // Dynamic import
+      TodoCli = TodoCli.default;
       TodoCli.init({
         target: ['client/**/*.{js,styl}', 'server/**/*.js'],
         verbose: false
