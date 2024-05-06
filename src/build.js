9608451f8788cbd1550a19e4e4a14bf79b780f5c
pieroblunda <pieroblunda@gmail.com>
2024-05-06 18:48:40 +0200

    Fix bug

--- src/build.js
+++ src/build.js
@@ -1,13 +1,14 @@
 import Framework from './framework.js';
+import StylusVespa from './stylus.server.model.js';
 
 class Build {
   
   static run() {
     console.log('Building...');
     Framework.setGlobalsVariables();
-    Framework.compileStylus();
     Framework.compileJs();
     Framework.compileMedia();
+    StylusVespa.compileStylus();
     console.log('Build done!');
   }
   
