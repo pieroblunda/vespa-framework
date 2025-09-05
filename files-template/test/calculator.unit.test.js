ff445859ff616d476db1b1642cab077f6766714a
pieroblunda <pieroblunda@gmail.com>
2025-09-05 17:17:51 +0200

    Ad support for test files

--- files-template/test/calculator.unit.test.js
+++ files-template/test/calculator.unit.test.js
@@ -4,6 +4,7 @@ import Calculator from "../server/models/calculator.server.model.js";
 
 describe('formatFileSize function', () => {
   it('1 must be equal to 1', () => {
-    assert.strictEqual(1, 1);
+    let sum = Calculator.sum(2,2);
+    assert.strictEqual(sum, 4);
   });
 });
